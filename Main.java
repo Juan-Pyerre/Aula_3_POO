572e233ed69660ef2efe51db045db49196cc50f8
Juan.777 <juan.pyerre777@gmail.com>
2026-03-03 20:35:33 -0300

    Update Main.java
    fix: Atualizando o codigo

--- Main.java
+++ Main.java
@@ -6,11 +6,12 @@ public class Main {
 		
 		Carro carro1 = new Carro("Fusca", 1980, "verde");
 		System.out.println("o marca do carro é " + carro1.getMarca());
-		
-		Trator trator1 = new Trator ("Renalt", 2000, "cinza");
-		System.out.println("a marca do trator é " + trator1.getMarca());
-		
 		System.out.println(carro1.Buzinar());
+		Trator trator1 = new Trator ("Renalt", 2000, "cinza", "Motor a dizel", "John Deere");
+		System.out.println("a marca do trator é " + trator1.getMarca());
+		System.out.println(trator1.Buzinar()); 
+		
+		
 	}
 
 }
