a7b5bab2b89da12071e6574539b2fd926e9a35a8
Juan.777 <juan.pyerre777@gmail.com>
2026-03-03 20:36:15 -0300

    Update Trator.java
    fix: Atualizando o codigo

--- Trator.java
+++ Trator.java
@@ -1,10 +1,33 @@
-package main;
+package main.java;
 
 public class Trator extends Carro {
+	private String motor;
+	private String modelo;
 
-	public Trator(String marca, int ano, String cor) {
+	public Trator(String marca, int ano, String cor, String motor, String modelo) {
 		super(marca, ano, cor);
-		// TODO Auto-generated constructor stub
+		this.motor=motor;
+		this.modelo=modelo;
 	}
-	
+	public String getMotor() {
+		return motor;
+	}
+
+	public void setMotor(String motor) {
+		this.motor = motor;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}		
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	@Override
+	public String Buzinar (){
+		return ("TCHUTCHUUUUUUUUUUUUUUUUUUUUU");
+
+	}
 }
